15e1e5001a164e4a185bebd7214371464f4b281c
Ashley <17520437@gm.uit.edu.vn>
2026-05-18 23:48:28 +0700

    SL-01| config org selection page, sign in page, sign up page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 "use client";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 
-import { Button } from "@/components/ui/button";
-import { use } from "react";
-import { toast } from "sonner";
-const Home = () => {
+const HomePage = () => {
   return (
-    <div>
-      <Button size="xs" onClick={() => toast.success("Hello world")}>
-        Hello
-      </Button>
+    <div className="flex min-h-screen items-center justify-center bg-background">
+      <div className="flex flex-col items-center justify-center gap-4 p-4">
+        <h1 className="text-4xl font-bold">Welcome to SpeechFlow </h1>
+        <p className="text-lg">SpeechFlow is a platform for...</p>
+        <div className="flex gap-4">
+          <UserButton />
+          <OrganizationSwitcher />
+        </div>
+      </div>
     </div>
   );
 };
-export default Home;
+export default HomePage;
